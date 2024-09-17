69d09fd409545dd4765890ace36c6b7bd5094dcb
Stefan <floydrise@gmail.com>
2024-09-17 11:33:26 +0300

    Finalised project for now

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
+/* generates three random numbers and assigns rock, paper or scissors to a variable */
 function getComputerChoice() {
-    let randomNumber = getRandomInt(3);
+    let randomNumber = Math.floor(Math.random() * 3);
     let returnVar;
     switch (randomNumber) {
         case 0:
@@ -15,8 +16,59 @@ function getComputerChoice() {
     return returnVar;
 }
 
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
+
+function getHumanChoice() {
+    const choice = window.prompt("Choose:");
+    return choice;
 }
 
-console.log(getComputerChoice());
+function playGame() {
+
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(humanChoice, computerChoice) {
+        let returnValue;
+        if (computerChoice === "rock" && humanChoice.toLowerCase() === "scissors") {
+            returnValue = "Computer wins";
+            computerScore++;
+        } else if (computerChoice === "paper" && humanChoice.toLowerCase() === "rock") {
+            returnValue = "Computer wins";
+            computerScore++;
+        } else if (computerChoice === "scissors" && humanChoice.toLowerCase() === "paper") {
+            returnValue = "Computer wins";
+            computerScore++;
+        } else if (computerChoice === "rock" && humanChoice === "rock") {
+            returnValue = "Tie";
+        } else if (computerChoice === "paper" && humanChoice === "paper") {
+            returnValue = "Tie";
+        } else if (computerChoice === "scissors" && humanChoice === "scissors") {
+            returnValue = "Tie";
+        } else {
+            returnValue = "Player wins";
+            humanScore++;
+        }
+        return returnValue;
+    }
+
+    let counter = 0;
+    while (counter < 5) {
+        const humanSelection = getHumanChoice().toLowerCase();
+        const computerSelection = getComputerChoice();
+        let result = playRound(humanSelection, computerSelection);
+        console.log(`Round ${counter + 1}: ${result}`);
+        console.log(`Score -> Player: ${humanScore} Computer: ${computerScore}`);
+        counter++;
+    }
+    console.log();
+    console.log(`Final score -> Player: ${humanScore} Computer: ${computerScore}`);
+    if (humanScore > computerScore) {
+        console.log("You win the game!")
+    } else if (computerScore > humanScore) {
+        console.log("Computer wins the game")
+    } else {
+        console.log("It's a tie");
+    }
+}
+
+playGame();
